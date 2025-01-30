2e4177799e270c9668a183dd9ee5a52db5fe9905
phantomrsw <phantomrsw@rambler.ru>
2025-01-30 12:20:48 +0300

    fixed error when specifying one file in parameters

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,7 @@ struct file_prop_t{
     std::string path;
     std::string name;
     std::string date;
-    std::string size;
+    size_t size{};
     std::string crc32;
     };
 
@@ -52,21 +52,60 @@ void showHelp(){
                 << "  fp file.txt"<< std::endl;
 }
 
+void parseFile(const std::filesystem::path &path, std::vector<file_prop_t> &file_prop){
+    if (is_regular_file(path)){
+
+        file_prop_t fp;
+
+        std::ifstream f;
+        f.open(path.string(), std::ios::binary);
+
+        if(!f.is_open())
+            std::cout << "Fail! File " << path.string() << " not exist!" << std::endl;
+
+        size_t size_file = file_size(path);
+
+        uint32_t crc{};
+        uint8_t buf[1];
+        for(int i = 0; i < size_file; i++)
+        {
+            f.read(reinterpret_cast<char *>(&buf), sizeof(buf));
+            crc = crcCycle(crc, buf, sizeof(buf));
+        }
+
+        fp.path = path.parent_path().string();
+        fp.name = path.filename().string();
+        fp.date = getFormattedFileTime(path,"%d.%m.%Y %H:%M:%S");
+        fp.size = file_size(path);
+
+        std::stringstream s;
+        s << std::uppercase << std::setfill('0') << std::setw(8) << std::hex << crc;
+        fp.crc32 = s.str();
+
+        std::cout   << "path = "   << fp.path
+                    << " name = "  << fp.name
+                    << " date = "  << fp.date
+                    << " size = "  << fp.size
+                    << " crc32 = " << fp.crc32
+                    << '\n';
+
+        file_prop.push_back(fp);
+    }
+}
+
 int main(int argc, char** argv)
 {
     std::cout   << "File Property utility" << " " << PROGRAM_VERSION << " " << __DATE__ << std::endl;
 
-    std::string dir;
+    std::filesystem::path path{};
 
     if((argc == 2 && strcmp(argv[1],"-h") == 0) || argc == 1){
         showHelp();
         return 0;
     }
     else{
-        dir = argv[1];
+        path = argv[1];
     }
-
-    std::filesystem::path path{dir};
 
     if (!exists(path)){
         std::cerr << "FAIL! File or directory " << path << " no exist!" << std::endl;
@@ -78,56 +117,22 @@ int main(int argc, char** argv)
     uint64_t size_sum{};
     crcInit();
 
-    for(auto& p: std::filesystem::recursive_directory_iterator(path)){
-
-        if (p.is_regular_file()){
-
-            file_prop_t fp;
-
-            std::ifstream f;
-            f.open(p.path().string(), std::ios::binary);
-            if(!f.is_open())
-                std::cout << "Fail! File not exist!" << std::endl;
-
-            size_t size_file = file_size(p.path());
-            size_sum += size_file;
-
-            uint32_t crc{};
-            uint8_t buf[1];
-            for(int i = 0; i < size_file; i++)
-            {
-                f.read(reinterpret_cast<char *>(&buf), sizeof(buf));
-                crc = crcCycle(crc, buf, sizeof(buf));
-            }
-
-            fp.path = p.path().parent_path().string();
-            fp.name = p.path().filename().string();
-            fp.date = getFormattedFileTime(p.path(),"%d.%m.%Y %H:%M:%S");
-            fp.size = std::to_string(file_size(p.path()));
-
-            std::stringstream s;
-            s << std::uppercase << std::setfill('0') << std::setw(8) << std::hex << crc;
-            fp.crc32 = s.str();
-
-            std::cout   << "path = "   << fp.path
-                        << " name = "  << fp.name
-                        << " date = "  << fp.date
-                        << " size = "  << fp.size
-                        << " crc32 = " << fp.crc32
-                        << '\n';
-
-            file_prop.push_back(fp);
-
+    if(is_directory(path)){
+        for(auto& p: std::filesystem::recursive_directory_iterator(path)){
+            parseFile(p.path(),file_prop);
         }
-
+    }
+    else{
+        parseFile(path,file_prop);
     }
 
     try
     {
-        CsvFile csv("report.csv"); // throws exceptions!
-        csv << "path" << "name" << "date" << "size" << "crc32" << endrow;// Header
+        CsvFile csv("report.csv");
+        csv << "path" << "name" << "date" << "size" << "crc32" << endrow;
         for(auto &p : file_prop){
-            csv << p.path << p.name << p.date << p.size << p.crc32 << endrow;// Data
+            csv << p.path << p.name << p.date << p.size << p.crc32 << endrow;
+            size_sum += p.size;
         }
         csv << "----" << "----" << "----" << "----" << "----" << endrow;
         csv << "Summary size" << "" << "" << size_sum << endrow;
